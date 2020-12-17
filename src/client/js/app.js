384511782269cebc8696d5025abefc30c4e60980
Andrew <zlogik@outlook.com>
2020-12-16 20:17:11 -0500

    Server is working

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,55 +1,39 @@
 let startDate = "";
 let endDate = "";
 let destination = "";
-let hoursTill = "";
+let daysTill = "";
 let duration = "";
+let d1 = "";
+let d2 = "";
 
-export const getReady = async () => {
+/*export const getReady = async () => {
   console.log('Getting ready to post....')
   
-};
+};*/
 
 export const formHandler = async (event) => {
   event.preventDefault();
-  const d = new Date();
-
-  // Data collected from user
+  
+  // Data collected from form
   startDate = new Date(document.getElementById('start').value);
   endDate = new Date(document.getElementById('end').value);
   destination = document.getElementById('dest').value;
-  duration = (endDate.getTime() - startDate.getTime())/60000/60/24;
-  hoursTill = (startDate.getTime() - d.getTime())/60000/60/24;
-     
-  // Dates used in results
-  
-  
-  const d1 = startDate.toUTCString().slice(0,17);
-  const d2 = endDate.toUTCString().slice(0,17);
-
-  //const owAPI = owURL+zip+owCountry+appid+owKey;
-    
-  // for testing
-  //console.log(d1);
-  //console.log(d2);
-  //console.log(startDate);
-  //console.log(endDate);
-
-  console.log('Form Handled!!')
 
   const dataForServer = {destination,startDate,endDate}
-
-
-  getReady()
-  .then(() => {
-    //console.log(data)
-    postData('http://localhost:8081/add', dataForServer)
-    console.log('Data posted. Updating UI....')
-  })
-  .then(() => {
-    updateUI()
-  })
-
   
+  console.log('Form Handled. Getting dates...')
+
+  await getDate()
+
+  console.log('Dates Handled. Posting data...')
+
+  await postData('http://localhost:8081/add', dataForServer )
+
+  const getData = await fetch(`http://localhost:8081/all`);
+
+  console.log('Data Posted.')
+
+  await updateUI()
   
   
 };
@@ -82,28 +66,54 @@ const updateUI = async () => {
   try{
       const allData = await request.json()
       
-      console.log(allData)
-
       //console.log(allData)
-       
-      document.getElementById("disDest").innerHTML = `Destination: ${destination}`;
-      document.getElementById("disStart").innerHTML = `Start Date: ${startDate.toUTCString().slice(0,17)}`
-      document.getElementById("disEnd").innerHTML = `End Date: ${endDate.toUTCString().slice(0,17)}`
+    
+      document.getElementById("disDest").innerHTML = `Destination: ${allData.destination}`;
+      document.getElementById("disStart").innerHTML = `Start Date: ${d1}`
+      document.getElementById("disEnd").innerHTML = `End Date: ${d2}`
       document.getElementById("disDur").innerHTML = `Trip Duration: ${(duration)} days`;
-      document.getElementById("disTill").innerHTML = `Countdown: About ${Math.ceil(hoursTill)} days`;
+      document.getElementById("disTill").innerHTML = `Countdown: About ${Math.ceil(daysTill)} day(s)`;
       
     }catch(error){
       console.log("updateUI error", error)
   } 
 }
 
+
+
+
+
+const getDate = async () =>{
+
+  let d = new Date();
+
+  duration = (endDate.getTime() - startDate.getTime())/60000/60/24;
+  daysTill = (startDate.getTime() - d.getTime())/60000/60/24;
+
+  d1 = `${startDate.getMonth()}/${startDate.getDate()}/${startDate.getFullYear()}`
+  d2 = `${endDate.getMonth()}/${endDate.getDate()}/${endDate.getFullYear()}`
+
+  let newDate = d.getMonth()+'/'+ d.getDate()+'/'+ d.getFullYear()
+};
+
+
+
+
+
+
+
+
 export const cancel = () => {
-  document.getElementById("disDest").innerHTML = '';
-  document.getElementById("disStart").innerHTML = '';
-  document.getElementById("disEnd").innerHTML = '';
-  document.getElementById("disDur").innerHTML = '';
-  document.getElementById("disTill").innerHTML = '';
-  alert("Your trip has been cancelled.")
-  
+  let qq = confirm("Are you sure you want to cancel your trip?")
+  if (qq == true){
+    document.getElementById("disDest").innerHTML = '';
+    document.getElementById("disStart").innerHTML = '';
+    document.getElementById("disEnd").innerHTML = '';
+    document.getElementById("disDur").innerHTML = '';
+    document.getElementById("disTill").innerHTML = '';
+    alert("Yourtrip has been cancelled.")
+  }else{
+
+  }
 };
 
