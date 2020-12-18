486ec80523a4bb5acfe48d133c25b89129fb2386
Andrew <zlogik@outlook.com>
2020-12-18 10:14:38 -0500

    jest and service worker

---
+++ __Test__/formHandler.test.js
@@ -0,0 +1,10 @@
+import { postData } from '../src/client/js/app';
+
+describe("Testing the dates functionality", () => {
+
+    test('this is getDate test', () => {
+        const testDate = 'Positive'
+        expect(postData).toBeDefined();
+      }); 
+
+})
